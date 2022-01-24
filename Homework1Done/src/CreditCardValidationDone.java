6f31fcd949a1b5da752456909c7ddbf72a8e354d
Hridaya Bijayananda:= <hrbcrrvp@gmail.com>
2022-01-24 14:43:44 -0700

    1st week's hw completed

---
+++ Homework1Done/src/CreditCardValidationDone.java
@@ -0,0 +1,14 @@
+/*
+ * Hridaya Bijayananda
+ * Due: 01/24/22
+ */
+public class CreditCardValidationDone 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println ( "The first week's homework where we had to validate credit card numbers is completed.");
+
+	}
+
+}
