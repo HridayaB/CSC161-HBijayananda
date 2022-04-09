fbfadce21f1ec0d7c18fccb9d0de5f14a3f02f30
Kishore Menezes <kishore.menezes@frontrange.edu>
2022-04-09 17:46:23 -0600

    Added test for node removal in TreeMap

---
+++ MyTreeMap/src/test/java/com/CSC161J/MyTreeMap/TreeMapTestEval.java
@@ -0,0 +1,57 @@
+package com.CSC161J.MyTreeMap;
+
+import java.util.Map;
+
+import edu.frontrange.csc161j.MyTreeMap.MyTreeMap.Node;
+
+public class TreeMapTestEval {
+
+	public static void main(String[] args) {
+		MyTreeMap<Integer, Integer> map = new MyTreeMap<>();
+		map.put(8, 8);
+		map.put(12, 12);
+		map.put(4, 4);
+		map.put(14, 14);
+		map.put(10, 10);
+		map.put(6, 6);
+		map.put(2, 2);
+		map.put(1, 1);
+		map.put(3, 3);
+		map.put(7, 7);
+		map.put(5, 5);
+		map.put(11, 11);
+		map.put(9, 9);
+		map.put(13, 13);
+		map.put(15, 15);
+
+		printInOrder(map);
+		
+		map.remove(5);
+
+		printInOrder(map);
+		
+		map.remove(6);
+
+		printInOrder(map);
+		
+		map.remove(3);
+
+		printInOrder(map);
+		
+		map.remove(4);
+
+		printInOrder(map);
+		
+		map.remove(8);
+		
+		printInOrder(map);
+		
+	}
+
+	private static void printInOrder(MyTreeMap<Integer, Integer> map) {
+		for(com.CSC161J.MyTreeMap.MyTreeMap.Node node: map) {
+			System.out.print(node.key.toString() + " ");
+		}
+		System.out.println();
+	}
+}
