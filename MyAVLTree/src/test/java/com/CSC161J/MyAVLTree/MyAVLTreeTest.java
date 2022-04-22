40f2b65266c470e16715f6498adff2c815f08fb1
Kishore Menezes <kishore.menezes@frontrange.edu>
2022-04-22 12:26:08 -0600

    Modified test

--- MyAVLTree/src/test/java/com/CSC161J/MyAVLTree/MyAVLTreeTest.java
+++ MyAVLTree/src/test/java/com/CSC161J/MyAVLTree/MyAVLTreeTest.java
@@ -16,18 +16,28 @@ public class MyAVLTreeTest {
 		map.put(5, 5);		// Needs LL rotation at node 25
 		printInOrder(map);
 		
+		System.out.println("After LL rotation at node 25");
+		printBalanceFactor(map);
+		
 		map.put(34, 34);
 		printInOrder(map);
 		
 		map.put(50, 50);     // Needs RR rotation at node 25 
 		printInOrder(map);
 		
+		System.out.println("After RR rotation at node 25");
+		printBalanceFactor(map);
+		
 		map.put(30, 30);	// Needs RL rotation at node 20
 		printInOrder(map);
+		
+		System.out.println("After RL rotation at node 25");
+		printBalanceFactor(map);
 		
 		map.put(10, 10);     // Needs LR rotation at node 20.
 		printInOrder(map);
 
+		System.out.println("After LR rotation at node 25");
 		printBalanceFactor(map);
 	}
 
@@ -47,17 +57,21 @@ public class MyAVLTreeTest {
 			}
 		}
 		
-		System.out.println("\nNode\tHeight\tBalance Factor");
+		System.out.println("\nNode\tHeight\tBalance Factor\tLeftChild\tRightChild");
 		for(Node node: map) {
 			if (minHeight == 1) {
 				System.out.println(node.key.toString() + "\t" + node.height + "\t"
 					+ ((node.right != null ? node.right.height : 0) 
-							- (node.left != null ? node.left.height : 0)));
+							- (node.left != null ? node.left.height : 0)) 
+								+ "\t\t" + (node.left != null ? node.left.key.toString() : "null") 
+										+ "\t\t" + (node.right != null ? node.right.key.toString() : "null"));
 			}
 			else {
 				System.out.println(node.key.toString() + "\t" + node.height + "\t"
 						+ ((node.right != null ? node.right.height + 1 : 0) 
-								- (node.left != null ? node.left.height + 1 : 0)));
+								- (node.left != null ? node.left.height + 1 : 0))
+								+ "\t\t" + (node.left != null ? node.left.key.toString() : "null") 
+								+ "\t\t" + (node.right != null ? node.right.key.toString() : "null"));
 			}
 		}
 		System.out.println();
